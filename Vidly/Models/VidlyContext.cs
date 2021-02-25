52a217e0c6beb2d95a90a04a39b6538eec1003ad
Ryan Flemming <rmf919@live.com>
2021-02-25 12:04:18 +1100

    Seed data for movies and customers

--- Vidly/Models/VidlyContext.cs
+++ Vidly/Models/VidlyContext.cs
@@ -11,6 +11,23 @@ namespace Vidly.Models
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Customer> Customers { get; set; }
 
+        // Seed initial data for Movies and Customers
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasData(
+                new Movie {Id = 1, Name = "Shrek"},
+                new Movie {Id = 2, Name = "Wall-e"}
+            );
+
+            modelBuilder.Entity<Customer>().HasData(
+                new Customer  { Id = 1, Name = "Bob Smith" },
+                new Customer { Id = 2, Name = "Mary Williams" }
+            );
+        }
+
+
+
+
         // Constructor injection
         public VidlyContext(DbContextOptions<VidlyContext> options) : base(options)
         {
