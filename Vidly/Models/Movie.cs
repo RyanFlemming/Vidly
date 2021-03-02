a930450600f25672c1db145050ae1e67f5a65c0c
Ryan Flemming <rmf919@live.com>
2021-03-03 10:52:27 +1100

    Add Date Added, Genre, Num movies in stock and release date properties to Movie class

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,5 +9,9 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public DateTime DateAdded { get; set; }
+        public byte NumInStock { get; set; }
     }
 }
