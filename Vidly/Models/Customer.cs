6552c9f9554ca457130777776a2ae855a542f6f8
Ryan Flemming <rmf919@live.com>
2021-03-03 09:57:05 +1100

    implement birthdate for customer

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -13,6 +13,7 @@ namespace Vidly.Models
         [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
+        public DateTime? Birthdate { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
     }
