23a21af72f1396d2a690368ef5f6078cef8f5acc
Ryan Flemming <rmf919@live.com>
2021-03-15 11:13:06 +1100

    Fix PK on Genre

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -13,7 +13,7 @@ namespace Vidly.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public Genre Genre { get; set; }
-        public string GenreId { get; set; }
+        public byte GenreId { get; set; }
         public DateTime ReleaseDate { get; set; }
         public DateTime DateAdded { get; set; }
         public byte NumInStock { get; set; }
